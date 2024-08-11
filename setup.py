2fb5686a85f54c37e11917a6da89ca5c83330bd4
abdullhateef@gmail.com <abdullhateef@gmail.com>
2024-08-11 11:49:21 +0100

    data ingestion added

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __version__ = "0.0.0"
 
 REPO_NAME = "end_to_end_classification"
 AUTHOR_USER_NAME = "iamlati"
-SRC_REPO = "cnnClassifier"
+SRC_REPO = "CnnClassifier"
 AUTHOR_EMAIL = "abdullhateef@gmail.com"
 
 
---
+++ src/CnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+import os
+os.chdir("../")
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("C:/Users/User/Desktop/ENDTOEND/end_to_end_classification/config/config.yaml")
+PARAMS_FILE_PATH = Path("C:/Users/User/Desktop/ENDTOEND/end_to_end_classification/params.yaml")
