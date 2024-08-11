61be4be4f6715d3f2e97ad911a30dd8b05b16b1d
abdullhateef@gmail.com <abdullhateef@gmail.com>
2024-08-11 18:12:00 +0100

    Model trainer adder

--- templates/main.py
+++ templates/main.py
@@ -3,6 +3,7 @@ os.chdir("C:/Users/User/Desktop/ENDTOEND/end_to_end_classification")
 from CnnClassifier import logger
 from CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -21,6 +22,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
