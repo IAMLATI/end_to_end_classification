159f455b94e58309bd48b3844727e5f6c509ba5b
abdullhateef@gmail.com <abdullhateef@gmail.com>
2024-08-11 15:56:05 +0100

    prepare base model added

--- templates/main.py
+++ templates/main.py
@@ -2,6 +2,7 @@ import os
 os.chdir("C:/Users/User/Desktop/ENDTOEND/end_to_end_classification")
 from CnnClassifier import logger
 from CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -13,3 +14,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
